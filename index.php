ece12852debfb6fa1f643640d5a4fcc27cd6c760
Kasia <tudiaproducts@gmail.com>
2015-08-11 13:13:51 -0500

    index.php
    committing one file

---
+++ index.php
@@ -0,0 +1,494 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59035705-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- Basic Page Needs
+
+================================================== -->
+
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title>TUDIA Products | Mobile Accesories</title>
+
+	<meta name="description" content="Phone cases and products">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="author" content="Matthew Main">
+
+
+<!-- CSS
+
+================================================== -->
+  
+  
+	<!-- Favicon Code -->
+	
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+	
+	<link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+	
+	<!-- End Favicon Code -->
+	
+	<!--<link rel="stylesheet" type="text/css" href="Styles/product.quickview.css"/>-->
+
+	<!--<link rel="stylesheet" type="text/css" href="Styles/social.css"/>-->
+
+	<link rel="stylesheet" type="text/css" href="Styles/styles.css"/>
+
+	<!--<link rel="stylesheet" type="text/css" href="css/skeleton.css"/>-->
+
+	<link rel="stylesheet" type="text/css" href="css/layout.css"/>
+
+	<!--<link rel="stylesheet" type="text/css" href="css/colorbox.css"/>-->
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css"/>
+	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
+	
+	<link rel="stylesheet" type="text/css" href="css/revision.css"/>
+	
+	<link rel="stylesheet" type="text/css" href="css/base.css"/>
+
+	<!--<link rel="stylesheet" type="text/css" href="css/galaxy.css"/>-->
+
+	<!--<link href='https://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,700,700italic' rel='stylesheet' type='text/css'>-->
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+
+	<!--MenuCool ZoomSlider Links-->
+
+	<script src="zoomSlider.js" type="text/javascript"></script>
+
+    <link href="themes/1/zoomslider.css" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" type="text/css" href="css/footer.css"/>
+
+
+	<!--MenuCool ZoomSlider Links End-->
+
+
+	<!--zoomslider from walt links-->
+
+
+		<!-- must have -->
+
+
+		<!--<link href="css/bannerscollection_zoominout.css" rel="stylesheet" type="text/css">
+
+
+		<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+
+
+		<link href='http://fonts.googleapis.com/css?family=Lato:400,700,700italic' rel='stylesheet' type='text/css'>
+
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
+
+
+		<script src="js/jquery.ui.touch-punch.min.js" type="text/javascript"></script>
+
+
+		<script src="js/bannerscollection_zoominout.js" type="text/javascript"></script>-->
+
+
+		<!-- must have -->
+
+
+	<!--zoomslider from walt links end-->
+
+	<!-- Favicons
+
+	================================================== -->
+	<link rel="shortcut icon" href="favicon.png">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+
+</head>
+
+
+<body class="preload">
+
+
+	<!-- Primary Page Layout
+
+	================================================== -->
+
+	<div id="preloader">
+
+		<div id="status">&nbsp;</div>
+
+	</div>	
+
+
+	<!-- Home
+
+	================================================== -->	
+
+	<div class="homewrap">
+
+		<div id="homebg">
+
+			<div id="home">
+
+				<div class="home-text">
+
+					<div class="home-text-top"> <br></div>
+
+					<div class="line"></div>
+
+					<div class="home-text-middle">Tudia</div>
+
+					<div class="line"></div>
+
+
+
+					<div class="cl-effect-18">
+
+<!--<div id="galaxy">  
+
+  <div class="bg"></div>
+
+  <div class="stars-back"></div>
+
+  <div class="stars-middle"></div>
+
+  <div class="stars-front"></div>
+
+  <div class="bg center"></div>
+
+</div>-->
+
+					<!--<a href="#profile" class="scroll">About Us</a>
+
+					<a href="#contact" class="scroll">Contact</a>
+
+					<a href="http://www.facebook.com/thepathfinderproject" data-hover="facebook" target="_blank">facebook</a>
+
+					<a href="https://twitter.com/PF_Project" data-hover="Twitter" target="_blank">Twitter</a>-->
+
+                    </div>
+
+				</div>
+
+			    	<div class="fade-up-arrow"><div id="arrow"><a href="#contact" class="scroll" >About Us</a></div></div>	
+
+			</div>
+
+		</div>
+
+  	</div>
+
+	
+	<div id="cbp-so-scroller" class="cbp-so-scroller">		
+
+	<!--START WHO WE ARE-->
+
+	<!--<div id="profile">
+
+		<div class="title">
+
+		<div class="container">
+
+			 <section class="cbp-so-section">
+
+				<div class="sixteen columns cbp-so-side cbp-so-side-left">
+
+					<img class="custom-heading" src="images/who.png" alt="Who We Are" />
+
+				</div>
+
+                 <div class="sixteen columns cbp-so-side cbp-so-side-right">
+
+					<h6>JOIN THE URGENT ADVANCE OF HUMANITY BEYOND EARTH.</h6>
+
+				</div>
+
+				<div class="sixteen columns cbp-so-side cbp-so-side-right">
+
+					<h6>THE MOST SIGNIFICANT <span>PROJECT</span> OF ITS KIND IN A <span>GENERATION.</span></h6>
+
+				</div>
+         
+			</section>
+
+            <br>
+
+            <br>
+
+            <br>
+
+			</div>
+
+		</div>
+
+	</div>-->
+
+	<!--END WHO WE ARE-->
+
+
+	<!--START WHAT THE FANS SAY-->
+
+       
+
+	<!--<div id="parallax-2" class="parallax" style="background-image: url(images/dfd.jpg);">
+
+		<div class="parallax-wrap"> 	
+
+					<h5><span>What The Fans Say</span></h5>         
+
+					<div class="slider1">
+
+						<div class="slide">
+
+							<div class="testimonial"> 
+
+								<h6>Duis in nibh id lorem pulvinar adipiscing. Nulla odio elit, rutrum at convallis in, rhoncus ac urna. Curabitur justo urna, congue quis volutpat sed.</h6>
+								<p>Sarah, GraphicRiver</p>
+
+							</div> 
+
+						</div>
+
+						<div class="slide">
+
+							<div class="testimonial"> 
+
+								<h6>Curabitur justo urna, congue quis volutpat sed. Duis in nibh id lorem pulvinar adipiscing. Nulla odio elit, rutrum at convallis in, rhoncus ac urna.</h6>
+								<p>John Doe, ThemeForest</p>
+
+							</div> 
+
+						</div>
+
+						<div class="slide">
+
+							<div class="testimonial"> 
+
+								<h6>Nulla odio elit, rutrum at convallis in, rhoncus ac urna. Curabitur justo urna, congue quis volutpat sed. Duis in nibh id lorem pulvinar adipiscing.</h6>
+								<p>Mike Smith, CodeCanyon</p>
+
+							</div> 
+
+						</div>
+
+					</div>	
+
+			<div class="clouds"></div>
+
+		</div>
+
+	</div>-->
+
+    <!--END WHAT THE FANS SAY-->
+
+
+	<!--START AEROSPACE DIV -->
+
+	<div id="aeroSpaceDiv">
+
+	<div id="singleproject" background="url(../images/debut_dark.png) repeat">
+
+		<div class="container">
+
+			<div class="singleproject1-body">	
+
+				<div class="jumbotron">
+
+					<h4>Our Philosophy</h4>
+
+					<input type="checkbox" id="read_more" role="button" style="visibility:hidden">
+
+					<label for="read_more" onclick="" ><span>Read More</span><span>Hide</span></label> 
+
+					<section>
+
+						<h5>Distinction</h5>
+						<p>TUDIA was established in order to make a product that stands out with a refined distinction from the generic cellphone cases saturating the market today. For inspiration, TUDIA looked to appeal to what modern consumers want most: a protective case that also offers slimness and style, thus not taking away from the natural beauty of their electronic devices.</p><br>
+
+						<h5>Quality</h5>
+						<p>TUDIA evaluates every element of our cases to determine how each feature will serve both the aesthetic and functional value of the product. Additionally, TUDIA maintains close professional relationships with our manufacturers to ensure only the highest-grade materials and craftsmanship are employed in the assembly of our products. TUDIA stands behind the quality of our products and will be glad to take responsibility for any manufacturing defects that fail to meet quality standards by offering a free replacement when needed.</p>
+						</p><br>
+
+					</section>
+
+					<section class="hidden">
+
+						<h5>Dedication</h5>
+						<p> TUDIA’s greatest strength is our profound dedication to our customers. We draw from our customers for inspiration; every case we design incorporates the valuable feedback we receive and we try our best to fulfill their needs and expectations. In addition, TUDIA serves our customers by providing prompt and courteous customer support; we do our utmost to respond to all customer inquiries within 24 hours. We strive to maintain such great support for our customers because TUDIA is thankful for their loyalty. It is  those who support us that we strive to impress, time and time again, with innovative designs, superior products, and outstanding customer service.</p>
+
+					</section>
+
+				</div>
+
+			</div>
+
+		</div>	
+
+	</div>	
+
+
+<!-- JAVASCRIPT
+
+================================================== -->
+
+<!--<script type="text/javascript" src="js/jquery.js"></script>
+
+<script type="text/javascript" src="js/modernizr.custom.js"></script>
+
+<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+
+<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+
+<script type="text/javascript" src="js/singleproject1.js"></script>-->
+
+<script type="text/javascript" src="js/revision.js"></script>
+
+	<!-- END OF AEROSPACE PANEL -->
+
+
+	<!--START Products List Scroll-->
+<div class="container-fluid productWrapper">
+		<div id="parallax-4" class="parallax layer" style="background-image: url(images/Optimized-parra1.jpg);">
+
+			<div class="parallax-wrap"> 	
+
+
+
+					<div class="sixteen columns">  
+
+						<h5><a href="http://tudiaproducts.mybigcommerce.com/"><span>Products</span></a></h5>         
+
+					</div>
+
+				
+				<div id="product-list"> 
+
+					<ul>
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-one">
+							<div class="product">
+							<a href="http://tudiaproducts.com/cygen"><div class="img-blog"><img src="images/cygen_2015_sm.jpg" alt="" /></div></a>
+							<h6>Cygen</h6>
+					
+							<a class="link-blog" href="/cygen">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-two">
+							<div class="product">
+							<a href="http://tudiaproducts.com/lite"><div class="img-blog"><img src="images/lite_HTC_M9_sm.jpg" alt="" /></div></a>
+							<h6>Lite</h6>
+							<a class="link-blog" href="/lite">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-three">
+							<div class="product">
+							<a href="http://tudiaproducts.com/wav-hybrid"><div class="img-blog"><img src="images/wav-nexus_sm.jpg" alt="" /></div></a>
+							<h6>Wav-Hybrid</h6>
+							<a class="link-blog" href="/wav-hybrid">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-four">
+							<div class="product">
+							<a href="http://tudiaproducts.com/arch"><div class="img-blog"><img src="images/arch_LG_G4_sm.jpg" alt="" /></div></a>
+							<h6>Arch</h6>
+							<a class="link-blog" href="/arch">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-five">
+							<div class="product">
+							<a href="http://tudiaproducts.com/ryft"><div class="img-blog"><img src="images/ryft_sm.jpg" alt="" /></div></a>
+							<h6>Ryft</h6>
+							<a class="link-blog" href="/ryft">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-six">
+							<div class="product">
+							<a href="http://tudiaproducts.com/omnix"><div class="img-blog"><img src="images/omnix_sm.jpg" alt="" /></div></a>
+							<h6>Omnix</h6>
+							<a class="link-blog" href="/omnix">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-seven">
+							<div class="product">
+							<a href="http://tudiaproducts.com/lucion"><div class="img-blog"><img src="images/lucion_sm.jpg" alt="" /></div></a>
+							<h6>Lucion</h6>
+							<a class="link-blog" href="/lucion">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-eight">
+							<div class="product">
+							<a href="http://tudiaproducts.com/klip/"><div class="img-blog"><img src="images/Teal_web_home.png" alt="" /></div></a>
+							<h6>Klip</h6>
+							<a class="link-blog" href="/klip/">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-nine">
+							<div class="product">
+							<a href="http://tudiaproducts.mybigcommerce.com/300mbps-wifi-wireless-n-usb-micro-mini-adapter/"><div class="img-blog"><img src="images/adapter_sm.jpg" alt="" /></div></a>
+							<h6>Adapter</h6>
+							<a class="link-blog" href="http://tudiaproducts.mybigcommerce.com/300mbps-wifi-wireless-n-usb-micro-mini-adapter/">Learn More</a>
+							</div>
+						</li>
+
+						<li class="col-xs-6 col-sm-4 col-md-3 col-lg-5ths product-ten">
+							<div class="product">
+							<a href="http://tudiaproducts.mybigcommerce.com/dual-port-usb-ultra-high-speed-simultaneous-charging-car-charger/"><div class="img-blog"><img src="images/charger_sm.jpg" alt="" /></div></a>
+							<h6>USB Charger</h6>
+							<a class="link-blog" href="http://tudiaproducts.mybigcommerce.com/dual-port-usb-ultra-high-speed-simultaneous-charging-car-charger/">Learn More</a>
+							</div>
+						</li>
+
+					</ul>
+					<!--<button>View More</button>
+					<div id="myelement">Click Me</div>-->
+				</div><!--Product List-->
+					
+
+
+
+
+	</div><!--Parallax-4-->
+
+	<div class="parallax-wrap">  
+		<h5><a href="http://tudiaproducts.mybigcommerce.com/"><span>Shop Now</span></a></h5>       
+	</div>
+
+</div>
+
+<?php
+
+include ("include/footer.php");
+
+?>
